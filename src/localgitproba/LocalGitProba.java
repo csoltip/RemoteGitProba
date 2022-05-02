dfc0cfe769718cb71b22971efcd99aa8f1b6a70f
hallgato <hallgato@mail.org>
2022-05-02 18:26:32 +0200

    static nincs

--- src/localgitproba/LocalGitProba.java
+++ src/localgitproba/LocalGitProba.java
@@ -3,10 +3,11 @@ package localgitproba;
 public class LocalGitProba {
 
     public static void main(String[] args) {
-        int osszeg = osszead(3, 5);
+        LocalGitProba lgp = new LocalGitProba();
+        int osszeg = lgp.osszead(3, 5);
     }
 
-    private static int osszead(int a, int b) {
+    private int osszead(int a, int b) {
         return a + b;
     }
     
