95c9d7f3049284c288097018c36c9b19c9445b6a
hallgato <hallgato@mail.org>
2022-05-02 17:47:29 +0200

    osszead(int, int)

--- src/localgitproba/LocalGitProba.java
+++ src/localgitproba/LocalGitProba.java
@@ -3,7 +3,11 @@ package localgitproba;
 public class LocalGitProba {
 
     public static void main(String[] args) {
-        
+        int osszeg = osszead(3, 5);
+    }
+
+    private static int osszead(int a, int b) {
+        return a + b;
     }
     
 }
